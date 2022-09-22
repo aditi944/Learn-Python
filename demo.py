775ffcb1a8026e551468c79567aca33468c3dec3
aditi944 <aditijha201200@gmail.com>
2022-09-22 22:51:52 +0530

    first

---
+++ demo.py
@@ -0,0 +1,31 @@
+print("hello")
+a=9
+b=5
+c=a+b
+print(c)
+
+#hello,this live is waste- comments
+
+#variables
+
+x=5
+y="aditi"
+print(y)
+
+#casting
+x=str(3)
+y=int(3)
+z=float(3)
+print(z)
+
+#get the type
+print(type(z))
+
+#variables name are case sensitive
+#strings can be declared either within single quotes or double quotes
+
+#unpacking a list
+
+fruits=["apples","banana", "pears"]
+
+print(fruits[1])
