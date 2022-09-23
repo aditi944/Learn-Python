d192188964e6a6fd60f14a4b9087887f9336722b
aditi944 <aditijha201200@gmail.com>
2022-09-23 14:47:32 +0530

    done

---
+++ List.py
@@ -0,0 +1,7 @@
+# Lists are used to store multiple items in a single variable.
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# List items are ordered, changeable, and allow duplicate values.
+
