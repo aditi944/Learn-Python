360ad3ffe6cabf07c2934090a073ead16dc1079a
aditi944 <aditijha201200@gmail.com>
2022-09-23 13:52:25 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+# python identity operators- is and is not
+
+# is- they two objects are same which means same value aas well as same memory location
+# is not- 
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+
+# returns True because z is the same object as x
+
+print(x is y)
+
+# returns False because x is not the same object as y, even if they have the same content
+
+print(x == y)
+
+# to demonstrate the difference betweeen "is" and "==": this comparison returns True because x is equal to y
